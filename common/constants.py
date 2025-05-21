c8c8675a73c83fa93ea67e5e8bba2d42bb5e402e
Norvik Alexian <norvik@ucraft.com>
2025-05-21 11:29:55 +0400

    created common python directory in order to store constants identifiers

---
+++ common/constants.py
@@ -0,0 +1,6 @@
+REGEX_PATTERN = r"^(SECTION\s+\d+|[A-Z\s]{5,})$"
+
+MARKDOWN_PATH = "../Port_Tariff.md"
+INDEX_PATH = "../data/port_tariff.index"
+KEYS_PATH = "../data/port_tariff_keys.pkl"
+PDF_PATH = "../Port Tariff.pdf"
---
+++ common/llm_constants.py
@@ -0,0 +1,3 @@
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+GEMINI_MODEL = "gemini-2.0-flash-001"
+TOP_K = 5  # number of chunks to retrieve
